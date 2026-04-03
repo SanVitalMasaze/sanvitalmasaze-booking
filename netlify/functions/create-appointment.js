836e8a9dac49b59ab16d5a2e5d490d1a4db9f74f
SanVitalMasaze <sanvitalmasaze@gmail.com>
2026-04-03 12:51:56 +0200

    Update create-appointment.js

--- netlify/functions/create-appointment.js
+++ netlify/functions/create-appointment.js
@@ -19,8 +19,10 @@ export const handler = async (event, context) => {
     }
 
     const supabase = createClient(
-      process.env.SUPABASE_URL,
-      process.env.SUPABASE_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+)
+
     )
 
     // 1) Preveri, ali je termin že zaseden
